ad8ec763df55b71df22b102ef7d5dac5f7cd5a5a
Gokhan_D <gdogan3746@gmail.com>
2022-07-21 21:37:53 +0200

    search and weather card component has created

---
+++ src/components/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '103d9176f2msha1fc2ddeb6c0c43p13a941jsn9b4a477ab8a9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
---
+++ src/components/current-weather/currentWeather.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./currentWeather.css";
+
+const CurrentWeather = () => {
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">Belgrade</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="paramater-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="paramater-label">Feels Like</span>
+            <span className="paramater-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="paramater-label">Wind</span>
+            <span className="paramater-value">2 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="paramater-label">Humidity</span>
+            <span className="paramater-value">22%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="paramater-label">Pressure</span>
+            <span className="paramater-value">22 hPa</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentWeather;
